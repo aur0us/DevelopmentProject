ebe6e8c129c8311f481e6a39c2480faec99c9bf4
Ali Hasan <73433325+aur0us@users.noreply.github.com>
2024-03-01 11:14:49 +0000

    Initial Replacer

---
+++ Service/HTMLParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HTMLProcessor.Service
+{
+    public class HTMLParser
+    {
+        public string ProcessHTML(string inputHTML, string searchString, string replacementString)
+        {
+            string pattern = Regex.Escape(searchString);
+            return Regex.Replace(inputHTML, pattern, replacementString);
+        }
+    }
+}
